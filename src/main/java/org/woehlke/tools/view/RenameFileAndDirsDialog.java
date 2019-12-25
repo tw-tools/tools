ccdb16b7baad423a5b8e00dc1f4058ec5d1933da
Thomas Woehlke <thomas.woehlke@gmail.com>
2019-12-25 15:01:39 +0100

    setting up

--- src/main/java/org/woehlke/tools/view/RenameFileAndDirsDialog.java
+++ src/main/java/org/woehlke/tools/view/RenameFileAndDirsDialog.java
@@ -4,23 +4,25 @@ import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import static javax.swing.BoxLayout.Y_AXIS;
 
 @Component
-public class RenameFileAndDirsDialog extends JDialog {
+public class RenameFileAndDirsDialog extends JDialog implements ActionListener {
 
     public RenameFileAndDirsDialog() {
         initUI();
     }
 
-    private JButton ok = new JButton("Start");
+    private JButton ok = new JButton("OK");
     private JButton cancel = new JButton("Cancel");
     private JTextField fieldDirectoryName = new JTextField("You must choose the Root Directory first");
     private JButton buutonDirectoryName = new JButton("Choose Root Directory");
 
-
     private JPanel buttonFooter = new JPanel();
+    private JSeparator separator = new JSeparator();
     private JPanel directoryChosser = new JPanel();
 
     private void initUI() {
@@ -33,11 +35,27 @@ public class RenameFileAndDirsDialog extends JDialog {
         directoryChosser.add(fieldDirectoryName);
         directoryChosser.add(buutonDirectoryName);
         rootPane.add(directoryChosser);
+        rootPane.add(separator);
         rootPane.add(buttonFooter);
+        ok.addActionListener(this);
+        cancel.addActionListener(this);
+        buutonDirectoryName.addActionListener(this);
         pack();
         setSize(600, 400);
         setLocationRelativeTo(null);
     }
 
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()== ok ) {
+            System.out.println("OK");
+        } else if (e.getSource()== cancel ) {
+            System.out.println("cancel");
+            dispose();
+        } else if (e.getSource()== buutonDirectoryName ) {
+            System.out.println("Starte");
+        }
+    }
 
 }
