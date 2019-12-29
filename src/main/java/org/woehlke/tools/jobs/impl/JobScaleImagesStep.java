673d32f885ee01ad64ead6ee924e7a628daecc34
thomaswoehlke <thomas.woehlkegmail.com>
2019-12-29 11:29:04 +0100

    handle Job Events

--- src/main/java/org/woehlke/tools/jobs/impl/JobScaleImagesStep.java
+++ src/main/java/org/woehlke/tools/jobs/impl/JobScaleImagesStep.java
@@ -4,11 +4,11 @@ import org.woehlke.tools.db.common.JobCase;
 
 public enum JobScaleImagesStep {
 
-    SCALE_JPG_IMAGES ("rename Files"),
+    SCALE_JPG_IMAGES ("scale jpg images"),
     SCALE_IMAGE( "scale one image"),
     TRAVERSE_DIRS( "traverse Dirs"),
     TRAVERSE_FILES( "traverse Files"),
-    SET_ROOT_DIRECTORY( "setRootDirectory");
+    SET_ROOT_DIRECTORY( "set Root Directory");
 
     JobScaleImagesStep(String humanReadable){
         this.humanReadable = humanReadable;
