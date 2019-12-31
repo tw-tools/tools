35f8484fb1c09f7e86a081791a6527bb0c9510f5
thomaswoehlke <thomas.woehlkegmail.com>
2019-12-31 05:29:24 +0100

    H2 Datebase

--- src/main/java/org/woehlke/tools/view/tabbedpane/JobTreePanel.java
+++ src/main/java/org/woehlke/tools/view/tabbedpane/JobTreePanel.java
@@ -8,6 +8,13 @@ import org.woehlke.tools.model.db.services.JobService;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeCellRenderer;
+import javax.swing.tree.TreePath;
+
+import java.awt.*;
 
 import static javax.swing.BoxLayout.Y_AXIS;
 import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS;
@@ -19,6 +26,29 @@ public class JobTreePanel extends JPanel {
     private final JobService jobService;
     private final String frameTitle;
 
+
+    JTextField textField = new JTextField();
+    JScrollPane scrollPane = new JScrollPane();
+
+    JTree tree;
+
+    MyRenderer renderer = new MyRenderer();
+
+    DefaultMutableTreeNode nba = new DefaultMutableTreeNode("National Basketball Association");
+
+    DefaultMutableTreeNode western = new DefaultMutableTreeNode("Western Conference");
+
+    DefaultMutableTreeNode pacific = new DefaultMutableTreeNode("Pacific Division Teams");
+
+    DefaultMutableTreeNode midwest = new DefaultMutableTreeNode("Midwest Division Teams");
+
+    DefaultMutableTreeNode denver = new DefaultMutableTreeNode("Denver");
+
+    DefaultMutableTreeNode eastern = new DefaultMutableTreeNode("Eastern Conference");
+
+    DefaultMutableTreeNode atlantic = new DefaultMutableTreeNode("Atlantic Division Teams");
+    DefaultMutableTreeNode central = new DefaultMutableTreeNode("Central Division Teams");
+
     @Autowired
     public JobTreePanel(ToolsApplicationProperties prop, JobService jobService) {
         this.prop = prop;
@@ -29,36 +59,77 @@ public class JobTreePanel extends JPanel {
     }
 
     private JPanel scrollPanePanel;
-    private JScrollPane scrollPane;
 
     private void initUI() {
-        BoxLayout layoutRenameFilesAndDirs = new BoxLayout(this, Y_AXIS);
-        setLayout(layoutRenameFilesAndDirs);
-        this.setName(frameTitle);
-        scrollPanePanel = new JPanel();
-        scrollPanePanel.setLayout( new BoxLayout(scrollPanePanel, Y_AXIS));
-        scrollPanePanel.setName(frameTitle);
-        int thickness = 10;
-        Border border =  BorderFactory.createEmptyBorder(thickness,thickness,thickness,thickness);
-        TitledBorder myTitledBorder =  BorderFactory.createTitledBorder(border,frameTitle);
-        scrollPanePanel.setBorder(myTitledBorder);
-        int zeilenanzahl = 30;
-        String [] spalten = {"spalte1", "spalte2", "spalte3", "spalte4", "spalte5"};
-        String [][] daten = new String [zeilenanzahl][5];
-        for(int i=0; i< zeilenanzahl; i++){
-            for(int k=0;k<spalten.length;k++){
-                daten[i][k]  = "wert der spalte "+k+" und zeile "+i;
+        nba.add(western);
+        nba.add(eastern);
+        western.add(pacific);
+        western.add(midwest);
+        eastern.add(atlantic);
+        eastern.add(central);
+        pacific.add(new DefaultMutableTreeNode("Los Angeles (Lakers)"));
+        pacific.add(new DefaultMutableTreeNode("Los Angeles (Clippers)"));
+        pacific.add(new DefaultMutableTreeNode("San Francisco"));
+        pacific.add(new DefaultMutableTreeNode("Seattle"));
+        pacific.add(new DefaultMutableTreeNode("Phoenix"));
+        pacific.add(new DefaultMutableTreeNode("Portland"));
+        pacific.add(new DefaultMutableTreeNode("Sacramento"));
+
+
+        midwest.add(new DefaultMutableTreeNode("Utah"));
+        midwest.add(new DefaultMutableTreeNode("San Antonio"));
+        midwest.add(new DefaultMutableTreeNode("Houston"));
+        midwest.add(new DefaultMutableTreeNode("Minnesota"));
+        midwest.add(new DefaultMutableTreeNode("Vancouver"));
+        midwest.add(new DefaultMutableTreeNode("Dallas"));
+
+        midwest.add(denver);
+        atlantic.add(new DefaultMutableTreeNode("Miami"));
+        atlantic.add(new DefaultMutableTreeNode("New York"));
+        atlantic.add(new DefaultMutableTreeNode("New Jersey"));
+        atlantic.add(new DefaultMutableTreeNode("Washington"));
+        atlantic.add(new DefaultMutableTreeNode("Orlando"));
+        atlantic.add(new DefaultMutableTreeNode("Boston"));
+        atlantic.add(new DefaultMutableTreeNode("Philadelphia"));
+
+
+        central.add(new DefaultMutableTreeNode("Chicago"));
+        central.add(new DefaultMutableTreeNode("Indiana"));
+        central.add(new DefaultMutableTreeNode("Charlotte"));
+        central.add(new DefaultMutableTreeNode("Atlanta"));
+        central.add(new DefaultMutableTreeNode("Cleveland"));
+        central.add(new DefaultMutableTreeNode("Detroit"));
+        central.add(new DefaultMutableTreeNode("Milwaukee"));
+        central.add(new DefaultMutableTreeNode("Toronto"));
+        tree = new JTree(nba);
+
+        setLayout(new BorderLayout());
+        tree.setCellRenderer(renderer);
+        tree.addTreeSelectionListener(new TreeHandler());
+        scrollPane.getViewport().add(tree);
+        add("Center", scrollPane);
+        add("South", textField);
+    }
+
+    public class TreeHandler implements TreeSelectionListener {
+
+        public void valueChanged(TreeSelectionEvent e) {
+            TreePath path = e.getPath();
+            String text = path.getPathComponent(path.getPathCount() - 1).toString();
+            if (path.getPathCount() > 3) {
+                text += ": ";
+                text += Integer.toString((int) (Math.random() * 50)) + " Wins ";
+                text += Integer.toString((int) (Math.random() * 50)) + " Losses";
             }
+            textField.setText(text);
         }
-        JTable jTable = new JTable(daten, spalten);
-        scrollPane = new JScrollPane();
-        scrollPane.add(jTable);
-        scrollPane.setViewportView(jTable);
-        scrollPane.setAutoscrolls(true);
-        scrollPane.setWheelScrollingEnabled(true);
-        scrollPane.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPanePanel.add(scrollPane);
-        this.setLayout( new BoxLayout(this, Y_AXIS));
-        this.add(scrollPanePanel);
+    }
+
+    class MyRenderer extends JLabel implements TreeCellRenderer {
+        public java.awt.Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected,
+                                                      boolean expanded, boolean leaf, int row, boolean hasFocus) {
+            setText(value.toString() + "                   ");
+            return this;
+        }
     }
 }
